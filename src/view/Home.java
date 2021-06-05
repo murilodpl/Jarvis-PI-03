a5c0c261c794f270ada802cbbf2939002163d02d
murilodpl <inayievmpl@gmail.com>
2021-06-05 15:27:37 -0300

    Home task table

--- src/view/Home.java
+++ src/view/Home.java
@@ -1,19 +1,38 @@
 package view;
 
+import java.awt.Color;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
 import java.awt.Font;
 import java.awt.GridLayout;
 import javax.swing.SwingConstants;
+import javax.swing.table.DefaultTableModel;
+
+import database.BD;
+
 import javax.swing.JButton;
 import javax.swing.JSplitPane;
 import javax.swing.JTabbedPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import java.awt.event.ActionListener;
+import java.sql.PreparedStatement;
+import java.util.Date;
+import java.awt.event.ActionEvent;
+import javax.swing.JTextField;
 
 public class Home {
 
 	JFrame frmHome;
+	private JTable tableTasks;
+	private DefaultTableModel model;
+	
+	Login lpage = new Login();
+	String orderBy = "";
 
 	/**
 	 * Launch the application.
@@ -26,7 +45,7 @@ public class Home {
 					window.frmHome.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
-				} 
+				}
 			}
 		});
 	}
@@ -36,6 +55,7 @@ public class Home {
 	 */
 	public Home() {
 		initialize();
+		atualizar();
 	}
 
 	/**
@@ -47,11 +67,171 @@ public class Home {
 		frmHome.setBounds(100, 100, 520, 532);
 		frmHome.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmHome.getContentPane().setLayout(null);
-		
+
 		JLabel lblNewLabel = new JLabel("Jarvis");
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel.setBounds(10, 11, 484, 46);
 		lblNewLabel.setFont(new Font("Franklin Gothic Medium", Font.PLAIN, 40));
 		frmHome.getContentPane().add(lblNewLabel);
+
+		JButton btnLogout = new JButton("Logout");
+		btnLogout.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				frmHome.dispose();
+				lpage.emailLogin = "";
+				lpage.frmLogin.setVisible(true);
+			}
+		});
+		btnLogout.setFont(new Font("Franklin Gothic Medium", Font.PLAIN, 14));
+		btnLogout.setBounds(344, 448, 150, 34);
+		frmHome.getContentPane().add(btnLogout);
+		btnLogout.setBackground(Color.PINK);
+
+		JButton btnCreateProj = new JButton("Criar Projeto");
+		btnCreateProj.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				frmHome.dispose();
+				//CadastroProjeto cProjpage = new CadastroProjeto();
+				//cProjpage.frmCadastro.setVisible(true);
+			}
+		});
+		btnCreateProj.setFont(new Font("Franklin Gothic Medium", Font.PLAIN, 14));
+		btnCreateProj.setBounds(10, 448, 150, 34);
+		frmHome.getContentPane().add(btnCreateProj);
+
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 68, 484, 184);
+		frmHome.getContentPane().add(scrollPane);
+
+		tableTasks = new JTable(model);
+		scrollPane.setViewportView(tableTasks);
+		tableTasks.setModel(new DefaultTableModel(
+			new Object[][] {
+			},
+			new String[] {
+				"C\u00F3digo Tarefa", "Nome Tarefa", "Descri\u00E7\u00E3o", "Data Inicio", "Data Final", "C\u00F3digo Projeto"
+			}
+		) {
+			Class[] columnTypes = new Class[] {
+				Integer.class, String.class, String.class, String.class, String.class, Integer.class
+			};
+			public Class getColumnClass(int columnIndex) {
+				return columnTypes[columnIndex];
+			}
+			boolean[] columnEditables = new boolean[] {
+				false, false, false, false, false, false
+			};
+			public boolean isCellEditable(int row, int column) {
+				return columnEditables[column];
+			}
+		});
+		tableTasks.getColumnModel().getColumn(0).setResizable(false);
+		tableTasks.getColumnModel().getColumn(0).setPreferredWidth(80);
+		tableTasks.getColumnModel().getColumn(1).setResizable(false);
+		tableTasks.getColumnModel().getColumn(1).setPreferredWidth(100);
+		tableTasks.getColumnModel().getColumn(2).setResizable(false);
+		tableTasks.getColumnModel().getColumn(2).setPreferredWidth(90);
+		tableTasks.getColumnModel().getColumn(3).setResizable(false);
+		tableTasks.getColumnModel().getColumn(3).setPreferredWidth(65);
+		tableTasks.getColumnModel().getColumn(4).setResizable(false);
+		tableTasks.getColumnModel().getColumn(4).setPreferredWidth(65);
+		tableTasks.getColumnModel().getColumn(5).setResizable(false);
+		tableTasks.getColumnModel().getColumn(5).setPreferredWidth(85);
+		scrollPane.setViewportView(tableTasks);
+		
+		JButton btnOrderByNome = new JButton("Nome");
+		btnOrderByNome.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				orderBy = "order by nameTarefa";
+				atualizar();
+			}
+		});
+		btnOrderByNome.setFont(new Font("Franklin Gothic Medium", Font.PLAIN, 14));
+		btnOrderByNome.setBounds(344, 285, 150, 34);
+		frmHome.getContentPane().add(btnOrderByNome);
+		
+		JButton btnOrderByCod = new JButton("C\u00F3digo Tarefa");
+		btnOrderByCod.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				orderBy = "order by codTarefa";
+				atualizar();
+			}
+		});
+		btnOrderByCod.setFont(new Font("Franklin Gothic Medium", Font.PLAIN, 14));
+		btnOrderByCod.setBounds(10, 285, 150, 34);
+		frmHome.getContentPane().add(btnOrderByCod);
+		
+		JLabel lblNewLabel_1 = new JLabel("Ordenar por:");
+		lblNewLabel_1.setFont(new Font("Franklin Gothic Medium", Font.PLAIN, 14));
+		lblNewLabel_1.setBounds(10, 263, 77, 16);
+		frmHome.getContentPane().add(lblNewLabel_1);
+		
+		JButton btnCdigoProjeto = new JButton("C\u00F3digo Projeto");
+		btnCdigoProjeto.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				orderBy = "order by codProjeto";
+				atualizar();
+			}
+		});
+		btnCdigoProjeto.setFont(new Font("Franklin Gothic Medium", Font.PLAIN, 14));
+		btnCdigoProjeto.setBounds(170, 285, 164, 34);
+		frmHome.getContentPane().add(btnCdigoProjeto);
+		
+		JButton btnCreateTarefa = new JButton("Criar Tarefa");
+		btnCreateTarefa.setFont(new Font("Franklin Gothic Medium", Font.PLAIN, 14));
+		btnCreateTarefa.setBounds(170, 448, 164, 34);
+		frmHome.getContentPane().add(btnCreateTarefa);
+	}
+
+	// Atualizar tabela
+	private void atualizar() {
+		model = (DefaultTableModel) tableTasks.getModel();
+		model.getDataVector().removeAllElements();
+		BD bd = new BD();
+
+		String sql = "SELECT codTarefa, nameTarefa, descTarefa, data_inic_tarefa, data_final_tarefa, codProjeto FROM tarefa where emailUser = ? " + orderBy;
+
+		boolean conn = false;
+		PreparedStatement pstm = null;
+
+		try {
+			// criar uma conex�o com o banco de dados
+			conn = bd.getConnection();
+
+			pstm = bd.con.prepareStatement(sql);
+			pstm.setString(1, lpage.emailLogin);
+
+			// execute the query, and get a java resultset
+			bd.rs = pstm.executeQuery();
+
+			// iterate through the java resultset
+			while (bd.rs.next()) {
+				int codTarefa = bd.rs.getInt("codTarefa");
+				String nameTarefa = bd.rs.getString("nameTarefa");
+				String descTarefa = bd.rs.getString("descTarefa");
+				Date data_inic_tarefa = bd.rs.getDate("data_inic_tarefa");
+				Date data_final_tarefa = bd.rs.getDate("data_final_tarefa");
+				int codProjeto = bd.rs.getInt("codProjeto");
+
+				// print the results
+				model.addRow(new Object[] { codTarefa, nameTarefa, descTarefa, data_inic_tarefa, data_final_tarefa,
+						codProjeto });
+			}
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, e);
+			e.printStackTrace();
+		} finally {
+			// fechar as conex�es
+			try {
+				if (pstm != null) {
+					pstm.close();
+				}
+				if (conn == true) {
+					bd.close();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
--- src/view/Login.java
+++ src/view/Login.java
@@ -27,6 +27,8 @@ public class Login {
 	private JTextField tfPass;
 	private JButton btnCadastrar;
 
+	public static String emailLogin = "";
+
 	/**
 	 * Launch the application.
 	 */
@@ -59,97 +61,94 @@ public class Login {
 		frmLogin.setBounds(100, 100, 520, 300);
 		frmLogin.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmLogin.getContentPane().setLayout(null);
-		
+
 		JLabel lblNewLabel_1 = new JLabel("E-mail");
 		lblNewLabel_1.setHorizontalAlignment(SwingConstants.LEFT);
 		lblNewLabel_1.setFont(new Font("Franklin Gothic Medium", Font.PLAIN, 14));
 		lblNewLabel_1.setBounds(10, 52, 103, 23);
 		frmLogin.getContentPane().add(lblNewLabel_1);
-		
+
 		JLabel lblNewLabel = new JLabel("Jarvis");
 		lblNewLabel.setBounds(10, 11, 484, 30);
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel.setFont(new Font("Franklin Gothic Medium", Font.PLAIN, 26));
 		frmLogin.getContentPane().add(lblNewLabel);
-		
+
 		tfEmail = new JTextField();
 		tfEmail.setBounds(10, 80, 484, 20);
 		frmLogin.getContentPane().add(tfEmail);
 		tfEmail.setColumns(10);
-		
+
 		lblNewLabel_2 = new JLabel("Senha");
 		lblNewLabel_2.setHorizontalAlignment(SwingConstants.LEFT);
 		lblNewLabel_2.setFont(new Font("Franklin Gothic Medium", Font.PLAIN, 14));
 		lblNewLabel_2.setBounds(10, 111, 103, 23);
 		frmLogin.getContentPane().add(lblNewLabel_2);
-		
+
 		tfPass = new JTextField();
 		tfPass.setColumns(10);
 		tfPass.setBounds(10, 139, 484, 20);
 		frmLogin.getContentPane().add(tfPass);
-		
+
 		JButton btnLogin = new JButton("Entrar");
 		btnLogin.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				BD bd = new BD();
 
-				String email = tfEmail.getText();
+				emailLogin = tfEmail.getText();
 				String pass = tfPass.getText();
-		        String sql = "SELECT * FROM usuario where emailUser = ? and passwordUser = ?";
+				String sql = "SELECT * FROM usuario where emailUser = ? and passwordUser = ?";
 
-		        boolean conn = false;
-		        PreparedStatement pstm = null;
+				boolean conn = false;
+				PreparedStatement pstm = null;
 
-		        try {
-		            // criar uma conex�o com o banco de dados
-		            conn = bd.getConnection();
+				try {
+					// criar uma conex�o com o banco de dados
+					conn = bd.getConnection();
 
-		            pstm = bd.con.prepareStatement(sql);
-		            pstm.setString(1, email);
-		            pstm.setString(2, pass);
-		            bd.rs = pstm.executeQuery();
+					pstm = bd.con.prepareStatement(sql);
+					pstm.setString(1, emailLogin);
+					pstm.setString(2, pass);
+					bd.rs = pstm.executeQuery();
 
-		            // iterate through the java resultset
-		            if (bd.rs.next()) {
-		                //if username and password are true
-		            	frmLogin.dispose();
-		            	Home hpage = new Home();
-		            	hpage.frmHome.setVisible(true);
-		            }else {
-		            	JOptionPane.showMessageDialog(null, "E-mail ou senha incorretos!");
-		            	tfEmail.setText("");
-		            	tfPass.setText("");
-		            	tfEmail.requestFocus();
-		            }
-		        } catch (Exception error) {
-		        	JOptionPane.showMessageDialog(null, error);
-		        } finally {
-		            // fechar as conex�es
-		            try {
-		                if (pstm != null) {
-		                    pstm.close();
-		                }
-		                if (conn == true) {
-		                    bd.close();
-		                }
-		            } catch (Exception error) {
-		            	JOptionPane.showMessageDialog(null, error);
-		            }
-		        }
-				
-				
+					// iterate through the java resultset
+					if (bd.rs.next()) {
+						// if username and password are true
+						frmLogin.dispose();
+						Home hpage = new Home();
+						hpage.frmHome.setVisible(true);
+					} else {
+						JOptionPane.showMessageDialog(null, "E-mail ou senha incorretos!");
+						tfPass.setText("");
+						tfPass.requestFocus();
+					}
+				} catch (Exception error) {
+					JOptionPane.showMessageDialog(null, error);
+				} finally {
+					// fechar as conex�es
+					try {
+						if (pstm != null) {
+							pstm.close();
+						}
+						if (conn == true) {
+							bd.close();
+						}
+					} catch (Exception error) {
+						JOptionPane.showMessageDialog(null, error);
+					}
+				}
 			}
 		});
 		btnLogin.setFont(new Font("Franklin Gothic Medium", Font.PLAIN, 14));
 		btnLogin.setBounds(10, 188, 223, 35);
 		frmLogin.getContentPane().add(btnLogin);
-		
+
 		btnCadastrar = new JButton("Cadastrar");
 		btnCadastrar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-	 			frmLogin.dispose();
-            	CadastroUsuario cpage = new CadastroUsuario();
-            	cpage.frmRegister.setVisible(true);
+				frmLogin.dispose();
+				CadastroUsuario cpage = new CadastroUsuario();
+				cpage.frmRegister.setVisible(true);
 			}
 		});
 		btnCadastrar.setFont(new Font("Franklin Gothic Medium", Font.PLAIN, 14));
